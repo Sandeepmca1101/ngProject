78f6cbcbd92d7b06c3cc3210cea9778c4680b41b
sandeepmca1101 <sandeepmca1101@gmail.com>
2018-11-20 23:45:33 +0530

    Components Added

--- ngProject/ngProject/ngProject/frontend/src/app/app.component.ts
+++ ngProject/ngProject/ngProject/frontend/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: '<h1>Template Added For Testing{{message}}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'frontend';
+  message='Welcome to the index page';
 }
---
+++ ngProject/ngProject/ngProject/frontend/src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+   username='defaultUsername1'
+   psw=''
+   error='Invalid Credentials'
+   invalidlogin=false
+   //Angular.Give me Router
+  //
+   constructor(private router :Router) { }
+
+  ngOnInit() {
+  }
+  handleLogin(){
+   if(this.username==="ok" && this.psw==="1"){
+     this.router.navigate(['welcome',this.username])
+     this.invalidlogin=false
+   }
+    else{
+     this.invalidlogin=true
+   }
+    console.log(this.username);
+    console.log(this.psw);
+  }
+
+}

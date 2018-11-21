119da14221d21c48fd2d8c8a9be5ccb07e2a69d6
sandeepmca1101 <sandeepmca1101@gmail.com>
2018-11-21 23:54:36 +0530

    TODOLIST

--- ngProject/ngProject/ngProject/frontend/src/app/welcome/welcome.component.ts
+++ ngProject/ngProject/ngProject/frontend/src/app/welcome/welcome.component.ts
@@ -11,7 +11,7 @@ export class WelcomeComponent implements OnInit {
 
   message="SomeWelcomeMEsg"
   name= ''
-  constructor(private route:ActivatedRoute) {
+  constructor(public route:ActivatedRoute) {
 
   }
 
